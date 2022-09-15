cab0f95ef2ea98367a1bcbfcf708272b8939c0fa
bhavi1902 <70758812+bhavi1902@users.noreply.github.com>
2022-09-15 19:51:29 +0530

    Update and rename Main.java to Seive_of_Eratosthenes.java

--- Seive_of_Eratosthenes.java
+++ Seive_of_Eratosthenes.java
@@ -1,29 +1,30 @@
+//1)Seive of eratosthenes 
 import java.util.*;
 class SieveOfEratosthenes {
     void sieveOfEratosthenes(int n)
     {
-        // Create a boolean array "prime[0..n]" and
-        // initialize all entries it as true. A value in
-        // prime[i] will finally be false if i is Not a
-        // prime, else true.
+        /* Creating a boolean array "prime[0..n]" and
+         and initializing all entries  as true. A value in
+         primeNumber[i] will finally be false if i is Not a
+         prime, else true.*/
         boolean primeNumber[] = new boolean[n + 1];
         for (int i = 0; i <= n; i++)
             primeNumber[i] = true;
  
         for (int p = 2; p * p <= n; p++) {
-            // If prime[p] is not changed, then it is a
-            // prime
+            /* If prime[p] is not changed, then it is a
+             prime*/
             if (primeNumber[p] == true) {
-                // Update all multiples of p greater than or
+                /* Update all multiples of p greater than or
                 // equal to the square of it numbers which
                 // are multiple of p and are less than p^2
-                // are already been marked.
+                // are already been marked.*/
                 for (int i = p * p; i <= n; i += p)
                     primeNumber[i] = false;
             }
         }
  
-        // Print all prime numbers
+        // Printing all prime numbers
         for (int i = 2; i <= n; i++) {
             if (primeNumber[i] == true)
                 System.out.print(i + " ");
